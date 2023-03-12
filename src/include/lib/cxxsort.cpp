2bb1996ef0610edd23d44ce0acb0179de120cb8f
robbert1978 <ngovinhhuyhl@gmail.com>
2023-03-12 17:01:08 +0700

    change log

--- src/include/lib/cxxsort.cpp
+++ src/include/lib/cxxsort.cpp
@@ -1,4 +1,4 @@
 #include <algorithm>
-template <typename T> void cxxSort(T *arr,long size){
+template <typename T> inline void cxxSort(T *arr,long size){
     std::sort(arr,arr+size);
 }
--- src/include/sort_alog.h
+++ src/include/sort_alog.h
@@ -2,7 +2,7 @@
 #include "lib/quicksort.cpp"
 #include "lib/mergesort.cpp"
 #include "lib/cxxsort.cpp"
-template <typename T> void heapSort(T* arr, long N);
+template <typename T> void heapSort(T* arr, long size);
 template <typename T> void quickSort(T* arr,long size);
 template <typename T> void mergeSort(T* arr,long size);
 template <typename T> void cxxSort(T *arr,long size);
